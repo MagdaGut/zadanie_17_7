c4c10c0510f2a3620f1f62b72d497ecd72271064
Magda Gutowska <m.gutowska0909@gmail.com>
2018-08-02 17:06:47 +0200

    Add files

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+	'GOOGLE_CLIENT_ID': '568183148656-85g6kr76cu7hgk6ruc7glgk44ahm9nfb.apps.googleusercontent.com',
+	'GOOGLE_CLIENT_SECRET': 'fZerylGSwFFmFwBV2hiev5GO',
+	'CALLBACK_URL': 'http://localhost:3000/auth/google/callback'
+}
